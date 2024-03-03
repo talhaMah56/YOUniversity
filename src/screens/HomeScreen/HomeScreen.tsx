a468c95b2eebf77d8982ab8b76bfa726d0005b83
Abdulrahman Abdulhamid <abdulrahman.t.abdulhamid@gmail.com>
2024-03-02 19:09:27 -0500

    skeleton for subject components

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { MainHero } from "../../Layout";
+import { SubjectComponent } from "../../components/SubjectComponent";
+
+import { MATH, CODING, CHEMISTRY, PHYSICS } from "../../router/private";
 
 export const HomeScreen: React.FC = () => {
   return (
@@ -8,6 +11,28 @@ export const HomeScreen: React.FC = () => {
       <div className="learn-more">
         <h1 className="heading-secondary">What is YOUniversity?</h1>
         <button className="learn-more-button">learn more</button>
+      </div>
+      <div className="subject-table">
+        <SubjectComponent
+          subjectRoute={MATH}
+          color="red"
+          description="this is math"
+        />
+        <SubjectComponent
+          subjectRoute={CODING}
+          color="gray"
+          description="this is coding"
+        />
+        <SubjectComponent
+          subjectRoute={CHEMISTRY}
+          color="yellow"
+          description="this is chemistry"
+        />
+        <SubjectComponent
+          subjectRoute={PHYSICS}
+          color="blue"
+          description="this is physics"
+        />
       </div>
     </>
   );
